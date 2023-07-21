03315186dc2c6bf0be254890fde0a2e1b56f532a
mohammed adel <hamadygg66@gmail.com>
2023-07-21 09:16:14 +0200

    Initial Commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by mohmmed adel on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
